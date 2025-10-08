b61e65520008738b5fdcc55ebeb9b341c4815270
Franco Aguilar <francoaguilar94.fa@gmail.com>
2025-10-08 12:08:12 -0300

    Update Index.html y styles.css

--- js/index.js
+++ js/index.js
@@ -1,18 +1,32 @@
 /* ----- Menu ----- */
-((d)=>{
-    const $btnMenu = d.querySelector(".menu-btn"),
-          $menu = d.querySelector(".menu");
+((d) => {
+	const $btnMenu = d.querySelector(".menu-btn"),
+		$menu = d.querySelector(".menu");
 
-    $btnMenu.addEventListener("click", e=>{
-        $btnMenu.firstElementChild.classList.toggle("none");
-        $btnMenu.lastElementChild.classList.toggle("none");
-        $menu.classList.toggle("is-active");
-    });
+	$btnMenu.addEventListener("click", (e) => {
+		$btnMenu.firstElementChild.classList.toggle("none");
+		$btnMenu.lastElementChild.classList.toggle("none");
+		$menu.classList.toggle("is-active");
+	});
 
-    d.addEventListener("click", e=>{
-        if(!e.target.matches(".menu a")) return false;
-        $btnMenu.firstElementChild.classList.remove("none");
-        $btnMenu.lastElementChild.classList.add("none");
-        $menu.classList.remove("is-active");
-    });
+	d.addEventListener("click", (e) => {
+		if (!e.target.matches(".menu a")) return false;
+		$btnMenu.firstElementChild.classList.remove("none");
+		$btnMenu.lastElementChild.classList.add("none");
+		$menu.classList.remove("is-active");
+	});
+})(document);
+
+((d) => {
+	const imagen = document.getElementById("logo_fab");
+
+	imagen.addEventListener("mouseover", () => {
+		imagen.classList.add("opacity");
+		imagen.src = "../assets/logo_baf_hover.png";
+	});
+
+	imagen.addEventListener("mouseout", () => {
+		imagen.classList.remove("opacity");
+		imagen.src = "../assets/logo_baf.png";
+	});
 })(document);
