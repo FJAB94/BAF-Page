71fb64df3ad85431174f48748c22e4b26faede73
Franco Aguilar <francoaguilar94.fa@gmail.com>
2025-10-11 16:37:59 -0300

    Update index.html, about.html, portfolio.html

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Any suggestions are welcome!
 I made this site with a minimalist vision so you will find these two pages:
 * **Home** - Here I've included a few sections, starting with a brief overview of my career as an artist and developer. Then we continue with my portfolio cards in a condensed version. Finally, a contact section.
 * **Portfolio** - Then we have a separate page for the portfolio where I put more details about the projects I develop. 
+* **About Me** - This don´t need any explanation, here i just tell my story.
--- js/index.js
+++ js/index.js
@@ -17,6 +17,8 @@
 	});
 })(document);
 
+/* ----- Logo ----- */
+
 ((d) => {
 	const imagen = document.getElementById("logo_fab");
 
@@ -31,5 +33,110 @@
 		imagen.classList.remove("opacity");
 		imagen.classList.remove("scale");
 		imagen.src = "../assets/logo_baf.png";
+	});
+})(document);
+
+/* ----- Contact Form ----- */
+((d) => {
+	const $form = d.querySelector(".contact-form"),
+		$response = d.querySelector(".contact-form-response");
+
+		const $loader = d.querySelector(".contact-form-loader");
+
+	$form.addEventListener("submit", (e) => {
+		e.preventDefault();
+		$loader.classList.remove("none");
+		/* ----- Codigo de Jon ----- */
+		/* fetch("http://formsubmit.co/francoaguilar94.fa@gmail.com", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+			},
+			body: new FormData(e.target),
+		})
+			.then((res) => (res.ok ? res.json() : Promise.reject(res)))
+			.then((json) => {
+				console.log(json);
+				location.hash = "#gracias";
+				$form.reset();
+			})
+			.catch((err) => {
+				console.log(err);
+				let message =
+					err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+				$response.querySelector(
+					"h3"
+				).innerHTML = `Error ${err.status}: ${message}`;
+				location.hash = "#gracias";
+			})
+			.finally(() => {
+				$loader.classList.add("none");
+				setTimeout(() => {
+					location.hash = "#close";
+				}, 3000);
+			}); */
+			/* ----- Codigo Copilot ------ */
+		fetch("https://formsubmit.co/francoaguilar94.fa@gmail.com", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+			},
+			body: new FormData(e.target),
+		})
+			.then((res) => {
+				if (res.ok) {
+					location.hash = "#gracias";
+					$form.reset();
+				} else {
+					throw res;
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				let message =
+					err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+				$response.querySelector(
+					"h3"
+				).innerHTML = `Error ${err.status}: ${message}`;
+				location.hash = "#gracias";
+			})
+			.finally(() => {
+				$loader.classList.add("none");
+				setTimeout(() => {
+					location.hash = "#close";
+				}, 4000);
+			});
+	});
+})(document);
+
+/* ----- Validation Form ----- */
+((d)=>{
+	const $form = d.querySelector(".contact-form"),
+		$inputs = d.querySelectorAll(".contact-form [required]");
+
+	$inputs.forEach((input) => {
+		const $span = d.createElement("span");
+		$span.id = input.name;
+		$span.textContent = input.title;
+		$span.classList.add("contact-form-error","none");
+		input.insertAdjacentElement("afterend", $span);
+	});
+
+	d.addEventListener("keyup", (e) => {
+		if (e.target.matches(".contact-form [required]")) {
+			let $input = e.target,
+				pattern = $input.pattern || $input.dataset.pattern;
+			if (pattern && $input.value !== "") {
+				let regex = new RegExp(pattern);
+				return !regex.exec($input.value)
+					? d.getElementById($input.name).classList.add("is-active")
+					: d.getElementById($input.name).classList.remove("is-active");
+			}
+			if (!pattern) {
+				return $input.value === ""
+					? d.getElementById($input.name).classList.add("is-active")
+					: d.getElementById($input.name).classList.remove("is-active");
+			}
+		}
 	});
 })(document);
